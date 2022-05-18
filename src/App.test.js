c1361829d35d68ee54b07fb977acf40efc2b4b62
David VanDusen <github@dvw3.com>
2022-05-18 12:33:00 -0700

    Install and run Prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders app name', () => {
+test("renders app name", () => {
   render(<App />);
   const appName = screen.getByText(/tessa/i);
   expect(appName).toBeInTheDocument();
