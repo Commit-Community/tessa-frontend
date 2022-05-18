f39739b0866dc7db1e7e948e6c33344b45abb0da
David VanDusen <github@dvw3.com>
2022-05-18 16:10:37 -0700

    No tests yet, but keep the pipeline step

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders app name", () => {
-  render(<App />);
-  const appName = screen.getByText(/tessa/i);
-  expect(appName).toBeInTheDocument();
-});
