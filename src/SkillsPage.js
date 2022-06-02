b7a3a03365f75b8896681b2ec5c691d051331dbe
David VanDusen <github@dvw3.com>
2022-06-02 16:58:24 -0700

    Show skeleton during loading on skills page

--- src/SkillsPage.js
+++ src/SkillsPage.js
@@ -5,8 +5,10 @@ import {
   Container,
   Grid,
   Link as MuiLink,
+  Skeleton,
   Typography,
 } from "@mui/material";
+import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
 
@@ -16,83 +18,86 @@ import useSession from "./useSession";
 
 const SkillsPage = () => {
   const { isAuthor } = useSession();
-  const { data: skills, isError, isLoading } = useQuery("skills", fetchSkills);
+  const { data: skills, isLoading } = useQuery("skills", fetchSkills);
   return (
     <Page>
       <Container sx={{ py: 6 }}>
-        {isLoading && "Loading..."}
-        {isError && "There was an error loading the page."}
-        {skills && (
-          <>
-            <Typography component="h1" variant="h3" mb>
-              Skills
-            </Typography>
-            <Typography component="p" variant="body1" sx={{ maxWidth: "30em" }}>
-              This page lists all of the essential skills that have been
-              documented in TESSA.
-            </Typography>
-            {skills && (
-              <Grid container spacing={2} sx={{ my: 3 }}>
-                {skills.map((skill) => (
-                  <Grid item key={skill.id} xs={12} md={6} lg={4}>
-                    <Card
-                      variant="outlined"
-                      sx={{ background: "rgba(0,0,0,0.5)", p: 3 }}
-                    >
-                      <Typography
-                        component="h2"
-                        variant="h6"
-                        lineHeight={1.25}
-                        mb
-                        minHeight="2.5em"
-                      >
-                        <MuiLink
-                          component={Link}
-                          to={`/skills/${skill.id}/`}
-                          underline="none"
-                          color="inherit"
-                        >
-                          {skill.name}
-                        </MuiLink>
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        mb={3}
-                        lineHeight={1.5}
-                        minHeight="4.5em"
-                      >
-                        {skill.description}
-                      </Typography>
-                      <Typography variant="button">
-                        <MuiLink
-                          component={Link}
-                          to={`/skills/${skill.id}/`}
-                          underline="none"
-                        >
-                          View skill
-                        </MuiLink>
-                      </Typography>
-                    </Card>
-                  </Grid>
-                ))}
-                {isAuthor && (
-                  <Grid item xs={12} md={6} lg={4}>
-                    <Box display="flex" justifyContent="center">
-                      <Button
-                        component={Link}
-                        to="/new-skill/"
-                        variant="contained"
-                        sx={{ my: 12 }}
-                      >
-                        + Add New Skill
-                      </Button>
-                    </Box>
-                  </Grid>
-                )}
+        <Typography component="h1" variant="h3" mb>
+          Skills
+        </Typography>
+        <Typography component="p" variant="body1" sx={{ maxWidth: "30em" }}>
+          This page lists all of the essential skills that have been documented
+          in TESSA.
+        </Typography>
+        <Grid container spacing={2} sx={{ my: 3 }}>
+          {isLoading ? (
+            <Fragment>
+              <Grid item xs={12} md={6} lg={4}>
+                <Skeleton height={220} variant="rectangular" />
               </Grid>
-            )}
-          </>
-        )}
+              <Grid item xs={12} md={6} lg={4}>
+                <Skeleton height={220} variant="rectangular" />
+              </Grid>
+              <Grid item xs={12} md={6} lg={4}>
+                <Skeleton height={220} variant="rectangular" />
+              </Grid>
+            </Fragment>
+          ) : (
+            skills.map((skill) => (
+              <Grid item key={skill.id} xs={12} md={6} lg={4}>
+                <Card variant="outlined" sx={{ p: 3 }}>
+                  <Typography
+                    component="h2"
+                    variant="h6"
+                    lineHeight={1.25}
+                    mb
+                    minHeight="2.5em"
+                  >
+                    <MuiLink
+                      component={Link}
+                      to={`/skills/${skill.id}/`}
+                      underline="none"
+                      color="inherit"
+                    >
+                      {skill.name}
+                    </MuiLink>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    mb={3}
+                    lineHeight={1.5}
+                    minHeight="4.5em"
+                  >
+                    {skill.description}
+                  </Typography>
+                  <Typography variant="button">
+                    <MuiLink
+                      component={Link}
+                      to={`/skills/${skill.id}/`}
+                      underline="none"
+                    >
+                      View skill
+                    </MuiLink>
+                  </Typography>
+                </Card>
+              </Grid>
+            ))
+          )}
+          {isAuthor && (
+            <Grid item xs={12} md={6} lg={4}>
+              <Box display="flex" justifyContent="center">
+                <Button
+                  component={Link}
+                  to="/new-skill/"
+                  variant="contained"
+                  sx={{ my: 11.5 }}
+                >
+                  + Add New Skill
+                </Button>
+              </Box>
+            </Grid>
+          )}
+        </Grid>
       </Container>
     </Page>
   );
