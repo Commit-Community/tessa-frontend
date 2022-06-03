92da1188abe9c6bc425470a311a0b47ec92c6253
David VanDusen <github@dvw3.com>
2022-06-03 14:18:56 -0700

    Improve main navigation for small screens

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import { Link as MuiLink, Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Footer = () => (
+  <Stack mt={12} mb={6} spacing={1} direction="row" justifyContent="center">
+    <Typography align="center" color="text.secondary" variant="body2">
+      © Commit Solutions Inc.
+    </Typography>
+    <MuiLink component={Link} to="/privacy-policy/" variant="body2">
+      Privacy Policy
+    </MuiLink>
+    <MuiLink component={Link} to="/terms-of-use/" variant="body2">
+      Terms of Use
+    </MuiLink>
+  </Stack>
+);
+
+export default Footer;
